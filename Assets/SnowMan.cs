53fb2ac0ea26ca8de80a2d1fd5fc9339c28e2adc
Julie <lacognata@intechinfo.fr>
2017-04-04 15:24:34 +0200

    Second commit

---
+++ Assets/SnowMan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowMan : MonoBehaviour {
+
+    public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float mouveHorizontal = Input.GetAxis("Horizontal");
+        float mouveVertical = Input.GetAxis("Vertical");
+
+        Vector2 mouvement = new Vector2(mouveHorizontal, mouveVertical);
+    }
+}
